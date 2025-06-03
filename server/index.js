75b9f315489a9062159c3501a61ef9fe7ac27b73
Muskan Verma <muskanverma0213@gmail.com>
2025-06-03 16:53:30 +0530

    Fix CORS

--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*', // or use specific origin below for more security
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}));
 app.use(express.json());
 
 // Debug middleware
