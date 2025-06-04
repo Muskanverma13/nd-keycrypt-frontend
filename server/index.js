85b18c6249b1bd21cae675d4303f226b70cf9da6
Muskan Verma <muskanverma0213@gmail.com>
2025-06-04 17:59:18 +0530

    new1

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 
 // Middleware
 app.use((req, res, next) => {
