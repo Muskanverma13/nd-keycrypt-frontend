82a3cdbf4c70fb9f926df577f62b8fdc598eedec
Muskan Verma <muskanverma0213@gmail.com>
2025-06-04 17:38:03 +0530

    new

--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: 'https://n-d-key-crypt.vercel.app',
-
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 }));
 app.use(express.json());
 
